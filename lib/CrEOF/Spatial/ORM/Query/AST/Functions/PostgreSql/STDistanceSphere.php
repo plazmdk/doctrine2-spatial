d870e46c3129f4f9254fcf5a53636a6ca8156c5b
Torben Pi Jensen <git@tjb.dk>
2018-06-09 21:49:49 +0200

    added mysql

--- lib/CrEOF/Spatial/ORM/Query/AST/Functions/PostgreSql/STDistanceSphere.php
+++ lib/CrEOF/Spatial/ORM/Query/AST/Functions/PostgreSql/STDistanceSphere.php
@@ -33,7 +33,7 @@ use CrEOF\Spatial\ORM\Query\AST\Functions\AbstractSpatialDQLFunction;
  */
 class STDistanceSphere extends AbstractSpatialDQLFunction
 {
-    protected $platforms = array('postgresql');
+    protected $platforms = array('postgresql', 'mysql');
 
     protected $functionName = 'ST_Distance_Sphere';
 
